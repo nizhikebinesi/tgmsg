03b9f7cd082ba473b79062c9d0b222a418bb8a0c
runotwo <d.antonov8499@gmail.com>
2019-10-01 20:25:33 +0300

    .

--- tgmsg/tg_client.py
+++ tgmsg/tg_client.py
@@ -78,8 +78,7 @@ class TelegramClient(object):
 
     def get_image(self, image_id):
         data = self.post_request('getFile', json.dumps({'file_id': image_id}))
-        raise Exception(json.dumps(data))
-        return requests.get(f'https://api.telegram.org/file/bot{self.token}/{data["file_path"]}').content
+        return requests.get(f'https://api.telegram.org/file/bot{self.token}/{data["result"]["file_path"]}').content
 
     def post_request(self, endpoint: str, data: str):
         if not isinstance(endpoint, str):
